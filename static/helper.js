2b7ad046aa8ed9bca89e1c5bb4601d67a3d52b77
Peter  Larson <larsonp@rose-hulman.edu>
2017-05-12 15:52:58 -0400

    Basic functionality

---
+++ static/helper.js
@@ -0,0 +1,23 @@
+console.log("Hello")
+
+var draw_rect = function(color){
+    var c = $("#place-canvas")[0];
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = color;
+    ctx.fillRect(0,0,10,10);
+}
+
+var update = function(){
+    $.getJSON("/canvas", function(data) {
+        var color = data[0][0]
+        draw_rect(color)
+    })
+}
+
+var set_color = function(new_color){
+    $.post("/update", {x: "0", y:"0", color: new_color}, 
+        function(returnedData){
+            console.log(returnedData);
+            update()
+        })
+}
