f18655a0d236431bd9282d67abd971fef7191d35
Peter  Larson <larsonp@rose-hulman.edu>
2017-05-14 20:36:37 -0400

    Fixed encoding issue

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 import redis
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+r = redis.StrictRedis(host='4902-11.csse.rose-hulman.edu', port=6379, db=0, \
+    password ='peterpeterpeter123123123123123123123123123213123123')
 
 app = Flask(__name__)
 
-#canvas = [["#FF0000"]]
 
 @app.route("/")
 def hello():
@@ -16,13 +16,15 @@ def update():
     x_coord = 0 #request.form['x']
     y_coord = 0 #request.form['y']
     color = request.form['color']
-    r.set(str(x_coord)+'-'+str(y_coord), color)
+    r.set(str(x_coord)+'-'+str(y_coord), color.encode('utf-8'))
     print("Setting: " + str(x_coord)+'-'+str(y_coord) )
     return "ok"
 
 @app.route("/canvas")
 def get_canvas():
-    return jsonify([[r.get('0-0')]])
+    value = r.get('0-0').decode('utf-8')
+    print("returning: "+value)
+    return jsonify([[value]])
 
 if __name__ == "__main__":
     app.run()
