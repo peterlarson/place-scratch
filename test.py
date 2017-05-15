6a9f0129aa1b81da098484ba1dd55a5890c992b8
Peter  Larson <larsonp@rose-hulman.edu>
2017-05-14 21:26:45 -0400

    Made board more than one pixel

--- test.py
+++ test.py
@@ -1,30 +1,55 @@
-from flask import Flask, request, jsonify, render_template
+'''
+test
+'''
+from flask import Flask, request, jsonify
+
 import redis
 
-r = redis.StrictRedis(host='4902-11.csse.rose-hulman.edu', port=6379, db=0, \
-    password ='peterpeterpeter123123123123123123123123123213123123')
+r = redis.StrictRedis(
+    host='4902-11.csse.rose-hulman.edu',
+    port=6379,
+    db=0,
+    password='peterpeterpeter123123123123123123123123123213123123')
 
 app = Flask(__name__)
 
+height = 4
+width = 4
+default_color = "#000000"
+
+def r_name(x,y):
+    return str(x) + '-' + str(y)
 
 @app.route("/")
 def hello():
     return app.send_static_file('blank.html')
+
 
 @app.route("/update", methods=['POST'])
 def update():
     x_coord = request.form['x']
     y_coord = request.form['y']
     color = request.form['color']
-    r.set(str(x_coord)+'-'+str(y_coord), color.encode('utf-8'))
-    print("Setting: " + str(x_coord)+'-'+str(y_coord) )
+    r.set(str(x_coord) + '-' + str(y_coord), color.encode('utf-8')) 
+    print("Setting: " + str(x_coord) + '-' + str(y_coord)) 
     return "ok"
 
-@app.route("/canvas")
+
+@app.route("/canvas") 
 def get_canvas():
-    value = r.get('0-0').decode('utf-8')
-    print("returning: "+value)
-    return jsonify([[value]])
+    canvas = []
+    for i in range(width): 
+        row = []
+        for j in range(height):
+            value = r.get(r_name(i,j))
+            if(value is None):
+                r.set(r_name(i,j), default_color.encode('utf-8'))
+                value = default_color
+            else: 
+                value = value.decode('utf-8')
+            row.append(value)
+        canvas.append(row)
+    return jsonify(canvas)
 
 if __name__ == "__main__":
     app.run()
